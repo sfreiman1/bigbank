b730490a825ad8da6fd053151a8751fae348aff7
sfreiman <scott@qwire.com>
2021-07-30 17:09:53 -0400

    BigBank - Finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import NavBar from "./components/navbar.js";
+import AllData from "./components/alldata.js";
+import CreateAccount from "./components/createaccount.js";
+import Deposit from "./components/deposit.js";
+import Home from "./components/home.js";
+import Withdraw from "./components/withdraw.js";
+import { UserProvider } from "./components/context.js";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <UserProvider>
+        <NavBar {...{ loggedIn, setLoggedIn }} />{" "}
+        <div className="container" style={{ padding: "20px" }}>
+          <Switch>
+            {!loggedIn && (
+              <Route path="/">
+                <Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+              </Route>
+            )}
+
+            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/deposit/" component={Deposit} />
+            <Route path="/withdraw/" component={Withdraw} />
+            <Route path="/alldata/" component={AllData} />
+
+            {/* If user clicks on brand and they are logged in, none of the routes trigger. This routes the user to the home page. */}
+            <Route path="/">
+              <Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+            </Route>
+          </Switch>
+        </div>
+      </UserProvider>
+    </HashRouter>
   );
 }
 
